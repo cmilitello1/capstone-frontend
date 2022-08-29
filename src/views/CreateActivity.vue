9380b06be18a05cba9460ca40dc3bb8a9f943c03
Caroline Militello <carolinemilitello@gmail.com>
2022-08-29 16:31:03 -0700

    init commit

---
+++ src/views/CreateActivity.vue
@@ -0,0 +1,20 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Create Activity page",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
